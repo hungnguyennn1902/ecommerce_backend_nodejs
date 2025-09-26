453499ed2beec2c376d44f024d3518469c1e43d4
Neo <neoo29v7@gmail.com>
2025-09-26 16:27:17 +0700

    config app

--- src/configs/config.mongodb.js
+++ src/configs/config.mongodb.js
@@ -11,16 +11,17 @@ const dev = {
 
 const pro = {
     app: {
-        port: process.env.PRO_APP_PORT || 3000
+        port: process.env.PRO_APP_PORT || 3052
     },
     db: {
         host: process.env.PRO_DB_HOST || 'localhost',
         port: process.env.PRO_DB_PORT || 27017,
-        name: process.env.PRO_DB_NAME || 'shopDev'
+        name: process.env.PRO_DB_NAME || 'shopPro'
     }
 }
 
 const config = { dev, pro }
+
 const env = process.env.NODE_ENV || 'dev'
 
 module.exports = config[env]
