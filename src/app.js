84e2a2aa1d72a0d0b31a87947ee73899b0717d22
Neo <neoo29v7@gmail.com>
2025-09-26 16:32:33 +0700

    async handler

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const morgan = require('morgan');
+const checkOverload = require('./helpers/check.connect').checkOverload;
 
 //init middleware
 app.use(morgan('dev'));
@@ -13,6 +14,10 @@ app.use(compression())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+//check overload
+// checkOverload();
+
 //init db
 require('./dbs/init.mongodb');
 
@@ -20,5 +25,19 @@ require('./dbs/init.mongodb');
 app.use('/', require('./routes/index'));
 
 //handling error
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
 
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500;
+    res.status(statusCode).json({
+        status: 'error',
+        code: statusCode,
+        stack: error.stack,
+        message: error.message || 'Internal Server Error',
+    });
+})
 module.exports = app;
